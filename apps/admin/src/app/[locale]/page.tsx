3bf51053aa2352c1de17aaaeda710f771138fbb2
unknown <hallousse.yehya@gmail.com>
2024-02-20 00:53:22 +0100

    feat(admin):add & config Internationalization

---
+++ apps/admin/src/app/[locale]/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
+export default function Index() {
+  const t = useTranslations('Index');
+
+  return (
+    <div className="text-center">
+      <h1>
+        <span>{t('tdaal')}</span>
+        <span> Hello there, </span>
+        Welcome admin 👋
+      </h1>
+    </div>
+  );
+}
