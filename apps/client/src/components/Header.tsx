281fd140b401a3eca63b8aa4a283835a155d4e7a
unknown <hallousse.yehya@gmail.com>
2024-03-24 02:27:27 +0100

    feat(client): header init

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,34 +1,40 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import logo from '../lib/assets/icons/tdaal-logo.png';
-import profile_pic from '../lib/assets/icons/profil-skeltone.jpg';
 import Image from 'next/image';
-import './header.scss';
+import Link from 'next/link';
+import { useState } from 'react';
+import profile_pic from '../lib/assets/icons/profil-skeltone.jpg';
+import logo from '../lib/assets/icons/tdaal-logo.png';
+
 function Header() {
   const t = useTranslations('Header');
   const [profile, setProfile] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 flex items-center justify-between w-full h-16 py-0 overflow-hidden border-b border-gray-300 px-7">
+    <nav className="fixed top-0 flex items-center justify-between w-full h-16 py-0 overflow-hidden border-b border-gray-300 px-7">
       {/* <div className="lg:max-w-[96%] max-w-full"> */}
-      <Link href="/home" className="hover:bg-[#052c4b] hover:text-[#fff] cursor-pointer block p-2 text-[#052c4b] transition capitalize" onClick={() => window.scroll(0, 0)}>
+      <Link
+        href="/home"
+        className="hover:bg-[#052c4b] hover:text-[#fff] cursor-pointer block p-2 text-[#052c4b] transition capitalize"
+        onClick={() => window.scroll(0, 0)}
+      >
         <Image
           src={logo}
           className="h-20 rounded-lg lg:h-12 w-9"
           alt="tdaal logo"
+          width={36}
+          height={48}
         />
       </Link>
       <div className="flex items-center justify-center">
-        <div className="button_p">
+        <div className="hover:bg-secondary hover:text-white hover:shadow-[0_7px_25px_rgba(0,0,0,0.08)] px-5 py-1 text-xs font-bold text-center uppercase block min-w-fit h-fit min-h-[30px] transition-[300ms] duration-[ease-in] rounded-sm cursor-pointer tr text-primary-foreground bg-primary">
           <Link href="/auth/sign_up">{t('createAccount')}</Link>
           <span>/</span>
 
           <Link href="/auth/sign_in">{t('login')}</Link>
         </div>
-        <Link href="/auth/sign_in" className="button_p">
+        <Link href="/auth/sign_in" className="hover:bg-secondary hover:text-white hover:shadow-[0_7px_25px_rgba(0,0,0,0.08)] px-5 py-1 text-xs font-bold text-center uppercase block min-w-fit h-fit min-h-[30px] transition-[300ms] duration-[ease-in] rounded-sm cursor-pointer tr text-primary-foreground bg-primary">
           {t('login')}
         </Link>
         <span className="border-l-2 border-l-[#052c4b] translate-x-1/2 h-6">
@@ -51,6 +57,8 @@ function Header() {
             src={profile_pic}
             className="w-8 h-8 mx-2 rounded-[50%]"
             alt="profile_pic"
+            width={32}
+            height={32}
           />
           {profile ? (
             <i className="fa-solid fa-caret-up"></i>
